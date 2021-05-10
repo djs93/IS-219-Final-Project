6fd16e0638826b598e25c0c4bd48542ecf55c995
Dale <djs93@njit.edu>
2021-05-10 13:38:04 -0400

    Attach Authorization header to API requests using JWT in cookies

--- src/js/getJSON.js
+++ src/js/getJSON.js
@@ -1,6 +1,25 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
 export default  function getJSON(url, callback) {
+    let token = 'Bearer ' + getCookie('jwtToken');
+    console.log(token);
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
+    xhr.setRequestHeader('Authorization',token);
     xhr.responseType = 'json';
     xhr.onload = function() {
         var status = xhr.status;
